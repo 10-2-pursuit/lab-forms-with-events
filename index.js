e9331b74c1159b1f0b87d3c3935184d902227682
Sung Yi <sy@Sungs-Air.fios-router.home>
2023-06-25 18:53:37 -0400

    init ver.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 console.log("Code your solution!")
+
+const result = document.getElementById("result");
+const form = document.querySelector(".form");
+
+form.addEventListener("submit", e => {
+    e.preventDefault();
+    let todo = e.target.todo.value;
+    const result = document.getElementById("result");
+    const li=document.createElement("li");
+    li.setAttribute("class", "todo_list");
+    li.textContent = todo;
+    li.addEventListener("click", event => {    
+        if(li.style.textDecoration == "line-through"){
+            li.style.textDecoration = null;
+        }
+        else{
+            li.style.textDecoration = "line-through";
+        }
+    })
+    result.appendChild(li);
+    form.reset();
+});
