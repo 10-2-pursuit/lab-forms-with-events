e4d0d95ac18495c63cbe85b53c18573e1a73f9a8
Areebur Rahman <102393675+AreeburR@users.noreply.github.com>
2023-06-26 14:46:49 -0400

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("Code your solution!")
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    let todo = event.target.todo.value;
+    const li = document.createElement("li");
+    li.textContent = todo;
+    const p = document.createElement("p");
+    const ul = document.querySelector("ul");
+
+    if (!todo) {
+        p.textContent = "Error! Todo can not be empty";
+        ul.prepend(p);
+    } else {
+        ul.append(li);
+    }
+    
+    form.reset();
+
+    let lis = document.querySelectorAll("li");
+    for (let li of lis) {
+        li.addEventListener("click", (event) => {
+            event.target.style.textDecoration = "line-through";
+        });
+    }
+});
