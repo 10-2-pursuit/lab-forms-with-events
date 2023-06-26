0c90b105361f5cb0634c3a9a5eb89816edededf2
Nugget-Meister <tremainecamacho@gmail.com>
2023-06-26 17:36:34 -0400

    start

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("Code your solution!")
+const form = document.querySelector("form")
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    let todo = event.target.todo.value
+
+    createList(todo)
+
+    form.reset()   
+});
+
+function createListTemplate(todo){
+    const li = document.createElement("li")
+    li.textContent = todo
+
+    return li
+}
+
+function createList(todo){
+    const li = createListTemplate(todo)
+    const ul = document.querySelector("ul")
+
+    ul.append(li)
+
+}
